466cff1c4386cf80f82b826344cb9f90be0e2ede
Oleh Khalieiev <oleh.khalieiev@nure.ua>
2022-08-30 23:23:53 +0300

    Format changes

--- lib/home/presentation/state/color_notifier.dart
+++ lib/home/presentation/state/color_notifier.dart
@@ -3,16 +3,15 @@ import 'package:test_task/common/utils/color_generation.dart';
 
 /// Class notifies its listeners of color changes.
 class ColorNotifier extends ChangeNotifier {
-
   Color _color = Colors.white;
 
   /// Gets color.
   Color get color => _color;
 
   /// Method that writes color.
-  void currentColor(){
-   final currentColor = ColorGeneration.generateRandomColor();
-   _color = currentColor;
-   notifyListeners();
+  void currentColor() {
+    final currentColor = ColorGeneration.generateRandomColor();
+    _color = currentColor;
+    notifyListeners();
   }
 }
